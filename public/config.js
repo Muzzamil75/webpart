bec4dd14f42667fd0d97f11add22072f2582d97e
Muzammil Ijaz <muzammil.ijaz@Cdocs.local>
2025-04-08 18:19:43 +0500

    company and forms api updated

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 var appConfigs = {
   // "COMPANIES_URL": "https://ddc4-companies.intelligize.net/api/companies",
-  "COMPANIES_URL":"https://ddc4-companies.intelligize.net/Companies/Search",
-  "FORMS_URL": "https://ddc4-sf.intelligize.net/build-204/api/Forms?appkey=sf",
+  "COMPANIES_URL":"https://ddc4-companies.intelligize.net/api/Companies/Search",
+  "FORMS_URL": "https://ddc4-sf.intelligize.net/build-039/api/Form?appkey=sf",
   "SEARCH_URL": "https://ddc4-sf.intelligize.net/build-204/api/search",
   "DOWNLOAD_URL": "https://ddc4-sf.intelligize.net/build-204/api/DocumentView/Download",
   "companyPageSize" : 10000
